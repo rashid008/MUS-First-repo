12491aca9df30acb8dbfc848cbe78de098ab2854
Rashid Azeem <124270608+rashid008@users.noreply.github.com>
2025-01-09 15:19:15 +0500

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -19,7 +19,7 @@ function Login({ setUserRole }) {
         event.preventDefault();
 
         try {
-            const response = await fetch('/api/login', {
+            const response = await fetch('${process.env.REACT_APP_API_URL}/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
