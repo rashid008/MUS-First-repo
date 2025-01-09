f2b4b378942e9dd0ae7d3220979c577a3aa42da2
Rashid Azeem <124270608+rashid008@users.noreply.github.com>
2025-01-09 17:33:50 +0500

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -20,7 +20,7 @@ function Login({ setUserRole }) {
 
     try {
         // Fetch API call to the backend
-        const response = await fetch(`/api/login`, { 
+        const response = await fetch(`https://victorious-calida-deadlycompliance420-43020d44.koyeb.app/api/login`, { 
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
